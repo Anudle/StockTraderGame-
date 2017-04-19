11eb91f8d79f15823b1ac925244d398cea70bf02
Anudle <AnuShrestha007@gmail.com>
2017-04-18 21:57:11 -0600

    init

---
+++ src/routes.js
@@ -0,0 +1,21 @@
+import Home from './components/Home.vue';
+import Stocks from './components/stocks/Stocks.vue';
+import Portfolio from './components/portfolio/Portfolio.vue';
+
+export const routes = [
+  {
+    path: '', name: 'home', components: {
+      default: Home
+    }
+  },
+  {
+    path: '/stocks', components: {
+      default: Stocks
+    }
+  },
+  {
+    path: '/portfolio', components: {
+      default: Portfolio
+    }
+  }
+]
